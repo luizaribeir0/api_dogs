80bb0f1a221a5ea74b20be69f7070734d151dd1e
Luiza Ribeiro <luiza@oneweb.com.br>
2023-07-18 11:09:44 -0300

    imagens contínuas

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+let timer;
+let deleteFirstPhotoDelay;
+
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await response.json();
     createBreedList(data.message);
 }
-
 start();
 
 function createBreedList(breedList) {
 document.getElementById("breed").innerHTML = `
     <select onchange="loadByBreed(this.value)">
         <option>Choose a dog breed</option>
+
         ${Object.keys(breedList).map(function (breed) {
             return `<option>${breed}</option>`
+
         }).join("")};
     </select>`;
 }
@@ -25,6 +29,28 @@ async function loadByBreed(breed) {
 }
 
 function createSlide(images) {
+    let currentPosition = 0;
+    clearInterval(timer);
+    clearTimeout(deleteFirstPhotoDelay);
+
     document.querySelector("div.show").innerHTML = `
-    <div class="slide" style="background-image: url('${images[0]}')"></div>`;
+    <div class="slide" style="background-image: url('${images[0]}')"></div>
+    <div class="slide" style="background-image: url('${images[1]}')"></div>`;
+
+    currentPosition += 2;
+    timer = setInterval(nextSlide, 3000);
+
+    function nextSlide() {
+        document.querySelector("div.show").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`);
+
+        deleteFirstPhotoDelay = setTimeout(function() {
+            document.querySelector("div.slide").remove();
+        },1000);
+
+        if (currentPosition + 1 >= images.length) {
+            currentPosition = 0;
+        } else {
+            currentPosition++;
+        }
+    }
 }
